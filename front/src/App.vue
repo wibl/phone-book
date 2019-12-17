d9c5256d6fbee5011094204bf5a6dbc6f26f9a35
wibl <035535@gmail.com>
2019-12-17 18:16:51 +0500

    Add load records action

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,3 +1,11 @@
+<script>
+export default {
+  created() {
+    this.$store.dispatch('loadRecords')
+  }
+}
+</script>
+
 <template>
   <div class="container" id="app">
     <h2>Phone Book</h2>
--- front/src/store/index.js
+++ front/src/store/index.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import RecordService from '../services/index.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    records: [{
+    records: [/*{
       id: 1, 
       name: "Petrov",
       number: "+79111111111"
@@ -14,7 +15,7 @@ export default new Vuex.Store({
       id: 2, 
       name: "Sidorov",
       number: "+79111111112"
-    }]
+    }*/]
   },
   mutations: {
     deleteRecord(state, id) {
@@ -40,11 +41,16 @@ export default new Vuex.Store({
     updateRecord ({ commit, state }, record) {
       if (record.id === -1) {
         let createdRecordId = state.records[state.records.length - 1].id + 1
-        // TODO: use spread operator
         commit('addNewRecord', {id: createdRecordId, name: record.name, number: record.number})
       } else {
         commit('updateRecord', record)
       }
+    },
+    async loadRecords ({ commit }) {
+      let {data} = await RecordService.getAllRecords()
+      data.forEach(rec => {
+        commit('addNewRecord', rec)        
+      });
     },
   },
 })
